4f01aff83d3be2d197c704d7dd6c05c5cafb287c
Melissa Donegan <melissadonegan25@gmail.com>
2020-09-29 17:23:33 -0400

    set up modal and container functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import ModalContainer from './Components/ModalContainer'
 
 function App() {
   return (
     <div className="App">
+      <ModalContainer />
     </div>
   );
 }
